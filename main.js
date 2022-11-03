e31f2cde16a2c64e23bfa4b857991e19edb158dd
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-11-03 13:14:09 -0400

    L2

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+  box: document.getElementById("big-black-box"),
 };
-console.log(DOMSelectors.button);
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "Ths is now a big red box";
+  text.style.fontSize = "40px";
+}
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
