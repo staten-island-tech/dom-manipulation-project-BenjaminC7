b5a8918d08c9567ef2086648c8d1257977af7aa2
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-11-04 13:15:14 -0400

    finished L4

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"),
+  points: document.querySelectorAll(".point"),
   box: document.getElementById("big-black-box"),
 };
 function backgroundAndText(background, text) {
   background.style.backgroundColor = "red";
-  text.innerHTML = "Ths is now a big red box";
+  text.textContent = "Ths is now a big red box";
   text.style.fontSize = "40px";
 }
 
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  let pointIndex = 1;
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.textContent = `Hello I am point ${pointIndex}`;
+      pointIndex++;
+    });
+  });
+}
+changeLi();
