6bc201415d99cfd415eb88cfcc1f373199fcb561
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-11-09 13:15:33 -0500

    started DOM manip

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  input: document.querySelector(`#input`),
+  box: document.getElementById(`questions`),
+  button1: document.getElementById(`btn1`),
+  button2: document.getElementById(`btn2`),
+  button3: document.getElementById(`btn3`),
+  input1: document.getElementById(`input1`),
+  input2: document.getElementById(`input2`),
+  input3: document.getElementById(`input3`),
 };
-//const cat = "meow";
-//DOMSelectors.box.insertAdjacentHTML("afterbegin", `<h1>We are ${cat}</h1>`);
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
-  DOMSelectors.input.value = "";
+
+DOMSelectors.button1.addEventListener("click", function () {
+  let input1 = DOMSelectors.input1.value;
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input1}</p>`);
+  DOMSelectors.input1.value = "";
+});
+DOMSelectors.button2.addEventListener("click", function () {
+  let input2 = DOMSelectors.input2.value;
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input2}</p>`);
+  DOMSelectors.input2.value = "";
+});
+DOMSelectors.button3.addEventListener("click", function () {
+  let input3 = DOMSelectors.input3.value;
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input3}</p>`);
+  DOMSelectors.input3.value = "";
 });
