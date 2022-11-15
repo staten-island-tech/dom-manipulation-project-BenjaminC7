0b31b6b419d151a13ca59deaa7552b23efb9dc79
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-11-15 13:14:47 -0500

    this works somehow

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const DOMSelectors = {
   objName: document.getElementById(`objName`),
   objAge: document.getElementById(`objAge`),
 };
-function create() {
+
+DOMSelectors.injector.addEventListener("click", function () {
   let name = DOMSelectors.input1.value;
   let age = DOMSelectors.input2.value;
   let favClr = DOMSelectors.input3.value;
@@ -19,17 +20,24 @@ function create() {
     favClr: `${favClr}`,
   };
   function inject(backgrond, name, age) {
-    backgrond.style.backgrondColor = `${profile.favClr}`;
-    name.textContent = `${profile.name}`;
-    age.textContent = `${profile.age}`;
+    backgrond.style.backgroundColor = `${profile.favClr}`;
+    name.textContent = `Hi! My name is ${profile.name}.`;
+    age.textContent = `I am ${profile.age} years old.`;
+    DOMSelectors.reseter.innerHTML = `<button type="button" class="button" id="reseter">
+    Click Here to Reset
+  </button>`;
   }
   function clear() {
     DOMSelectors.input1.value = "";
     DOMSelectors.input2.value = "";
     DOMSelectors.input3.value = "";
   }
-  DOMSelectors.injector.addEventListener("click", function clear() {
-    inject(DOMSelectors.newObj, DOMSelectors.objName, DOMSelectors.objAge);
-    clear();
-  });
-}
+  inject(DOMSelectors.newObj, DOMSelectors.objName, DOMSelectors.objAge);
+  clear();
+});
+DOMSelectors.reseter.addEventListener("click", function () {
+  DOMSelectors.newObj.style.backgroundColor = ``;
+  DOMSelectors.objName.textContent = ``;
+  DOMSelectors.objAge.textContent = ``;
+  DOMSelectors.reseter.innerHTML = ``;
+});
